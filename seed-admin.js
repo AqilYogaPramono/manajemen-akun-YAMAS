a552c66b2b4dfa3277885f07c695bdf23c76e11b
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-11-13 23:26:28 +0700

    fix: seed-admin

--- seed-admin.js
+++ seed-admin.js
@@ -8,9 +8,10 @@ async function seedAdmin() {
         if (rows.length > 0) {
             console.log('Admin sudah ditambahkan sebelumnya.')
         } else {
-            const hashedPassword = await bcrypt.hash(process.env.KATA_SANDI_ADMIN, 10)
+            const hashedPassword = bcrypt.hashSync(process.env.KATA_SANDI_ADMIN, 10)
             await connection.query(`INSERT INTO admin (nama, nomor_admin, kata_sandi) VALUES (?, ?, ?)`, [ process.env.NAMA_ADMIN, process.env.NOMOR_ADMIN, hashedPassword ])
         }
+        console.log('Akun Admin berhasil ditambahkan.')
     } catch (err) {
         console.error(err)
     } finally {
